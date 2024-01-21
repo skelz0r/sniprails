fb66ef50bfc45c98f81f3b3140c5b2e97f1d3c6a
Loïc Delmaire <loic.delmaire@gmail.com>
2024-01-21 21:19:01 +0100

    Scout commit

--- test/models/generator_test.rb
+++ test/models/generator_test.rb
@@ -2,6 +2,6 @@ require "test_helper"
 
 class GeneratorTest < ActiveSupport::TestCase
   test 'has valid factories' do
-    assert build(:generator).valid? == true
+    assert build(:generator).valid?
   end
 end
